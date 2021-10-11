8118f7c7059456982635b468de32eb7b4d053c51
Earthquake-008 <61614229+Earthquake-008@users.noreply.github.com>
2021-10-11 14:57:57 +0700

    update commend

--- src/main/scala/lectures/part1basics/CBNvsCBV.scala
+++ src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -6,18 +6,21 @@ object CBNvsCBV extends App {
     println("by value: " + 1257387745764245L)
     println("by value: " + 1257387745764245L)
   }
-
+// x: long -> call by value -> value is computed before call 
+// same value used everywhere
   def calledByName(x: => Long): Unit = {
     println("by name: " + System.nanoTime())
     println("by name: " + System.nanoTime())
   }
-
+// x=> long -> call by name -> this is the expression (value that can compute) , so the expression is passed literally
+// expression is evaluated at every use with in
   calledByValue(1257387745764245L)
   calledByName(System.nanoTime())
 
   def infinite(): Int = 1 + infinite()
-  def printFirst(x: Int, y: => Int) = println(x)
-
+  def printFirst(x: Int, y: >= Int) = println(x)
+// if change y=> to y: it will stack overflow because there valued is computed and die 
+// this can be run with call by name because y is not called cause expression is not passed by 
   //  printFirst(infinite(), 34) // stack overflow
   printFirst(34, infinite())
 }
